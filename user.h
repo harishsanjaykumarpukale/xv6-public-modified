e51eaec59e2e11ccbe56673af918b1b41f8fc2dd
Harish Sanjaykumar Pukale <46403164+harishsanjaykumarpukale@users.noreply.github.com>
2019-10-22 20:30:54 +0530

    user.h

--- user.h
+++ user.h
@@ -23,7 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-
+int cps(void);
+int chpr(int ,int );
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
